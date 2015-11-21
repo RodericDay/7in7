38a6cb1a63699d02a6da0cabd6c5837eedaf1152
Roderic Day <roderic.day@gmail.com>
2015-11-21 18:54:27 -0500

    Scala Traits

--- Scala/do2.scala
+++ Scala/do2.scala
@@ -1,23 +1,36 @@
-var text = "The name is Bond, James Bond."
+class Script(sourcePath:String) {
 
-def fromFile(path:String) : Map[String, String] = {
-    var map = Map.empty[String, String]
-    var pairs = io.Source.fromFile(path).mkString.split('\n').map(_.split(':'))
-    pairs.foreach { pair => map += Tuple2(pair(0), pair(1)) }
-    return map
+    val lines = io.Source.fromFile(sourcePath).mkString.split('\n')
+
+    def display() = lines.map(println)
+
+    def length():Int = return lines.foldLeft(0)((sum, line) => sum+line.length)
+
 }
 
-def translate(text:String, pairs:Map[String, String]) : String = {
-    // probably extremely inefficient. use regex?
-    var copy = text
-    pairs.foreach { pair => copy = copy.replace(pair._1, pair._2) }
-    return copy
+trait Censored {
+
+    var pairs = Map.empty[String, String]
+    io.Source.fromFile("do2-censored.txt")
+             .mkString.split('\n').map(_.split(':'))
+             .foreach { pair => pairs += Tuple2(pair(0), pair(1)) }
+
+    def translate(text:String) : String = {
+        // probably extremely inefficient. use regex?
+        var copy = text
+        pairs.foreach { pair => copy = copy.replace(pair._1, pair._2) }
+        return copy
+    }
+
 }
 
-def length(script:List[String]) : Integer = {
-    // use fold to check size of text
-    return script.foldLeft(0)( (sum, line) => sum + line.length )
+class CensoredScript(sourcePath:String) extends Script(sourcePath) with Censored {
+
+    override def display() = lines.map(translate).map(println)
+
 }
 
-val pairs = fromFile("do2.txt")
-println( translate(text, pairs) )
+val script = new Script("do2-script.txt")
+script.display
+val censoredScript = new CensoredScript("do2-script.txt")
+censoredScript.display
